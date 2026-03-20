63832f121fb2bfe31e5b1149f4699b0d20af9697
Palla Satteraju <palla.satteraju.met21@itbhu.ac.in>
2026-03-21 00:14:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='Medical-Chatbot',
+    version='0.1.0',
+    author='Palla Satteraju',
+    author_email='satterajupalla@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
